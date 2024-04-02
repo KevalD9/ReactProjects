1488cc5bbacc7aa1ae81b3b2f1ef9f74a422ba26
Keval Dobariya <kevaldobariya777@gmail.com>
2024-04-01 21:07:07 -0400

    Basic React Projects

---
+++ react-projects/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Accordian from "./components/accordin";
+import RandomColor from "./components/random-color";
+
+function App() {
+
+  return (
+    <>
+      {/* Accordin Component */}
+      <Accordian/>
+
+      {/* Random Color Generator */}
+      <RandomColor/>
+    </>
+  )
+}
+
+export default App
